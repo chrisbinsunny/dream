8f58c4b34b93a91e690131cdc737ce2d38465eaf
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-10-17 13:08:03 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Color Finder',
+      theme: ThemeData.dark(),
       home: HomePage(),
     );
   }

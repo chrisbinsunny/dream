894bdf29545a4dc926352405b37f4152291fab29
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-10-18 00:50:55 +0530

    Update image view

--- lib/imageView.dart
+++ lib/imageView.dart
@@ -2,19 +2,22 @@ import 'dart:developer';
 
 import 'package:color_finder/sizes.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import 'colorDetails.dart';
 import 'dropFile/dropFile.dart';
 
 class ImageViewer extends StatefulWidget {
-  const ImageViewer({Key? key, required this.file}) : super(key: key);
-  final DroppedFile? file;
+  const ImageViewer({Key? key,}) : super(key: key);
   @override
   State<ImageViewer> createState() => _ImageViewerState();
 }
 
 class _ImageViewerState extends State<ImageViewer> {
+  DroppedFile? file;
   @override
   Widget build(BuildContext context) {
+    file= Provider.of<ColorDetails>(context, listen: true).getFile;
     return Container(
       width: screenWidth(context, mulBy: 0.35),
       height: screenHeight(context, mulBy: 0.4),
@@ -22,7 +25,7 @@ class _ImageViewerState extends State<ImageViewer> {
       decoration: const BoxDecoration(
         color: Colors.blue
       ),
-      child: (widget.file == null)?
+      child: (file == null)?
       const Center(
           child: Text(
             "No Preview",
@@ -30,14 +33,13 @@ class _ImageViewerState extends State<ImageViewer> {
           )):
       GestureDetector(
         onTapDown: (tap){
-          
+          Provider.of<ColorDetails>(context, listen: false).setCoordinates(tap.localPosition);
         },
         onPanUpdate: (tap){
-          log("Global1 "+tap.globalPosition.toString());
-          log("Local1 "+tap.localPosition.toString());
+          Provider.of<ColorDetails>(context, listen: false).setCoordinates(tap.localPosition);
         },
         child: Image.network(
-        widget.file!.url,
+        file!.url,
         fit: BoxFit.contain,
     ),
       ),

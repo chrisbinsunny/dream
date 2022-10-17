a0081a51eb04d3e95b45df1c6b1feee0fd9f6e53
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-10-17 23:42:42 +0530

    Update colorCode.dart

--- lib/colorCode.dart
+++ lib/colorCode.dart
@@ -1,6 +1,8 @@
 
+import 'package:color_finder/colorDetails.dart';
 import 'package:color_finder/sizes.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ColorCode extends StatefulWidget {
   const ColorCode({Key? key}) : super(key: key);
@@ -15,8 +17,15 @@ class _ColorCodeState extends State<ColorCode> {
     return Container(
       width: screenWidth(context, mulBy: 0.35),
       height: screenHeight(context, mulBy: 0.4),
+      alignment: Alignment.center,
       decoration: const BoxDecoration(
           color: Colors.grey
+      ),
+      child: Text(
+        "Coordinates: ${Provider.of<ColorDetails>(context, listen: true).getCoordinates}",
+        style: TextStyle(
+          color: Colors.black
+        ),
       ),
     );
   }

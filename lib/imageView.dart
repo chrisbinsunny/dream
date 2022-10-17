a0c690abc15a1f3db6281c03e0edb31907a9345b
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-10-17 14:48:33 +0530

    Create imageView.dart

---
+++ lib/imageView.dart
@@ -0,0 +1,34 @@
+import 'package:color_finder/sizes.dart';
+import 'package:flutter/material.dart';
+
+import 'dropFile/dropFile.dart';
+
+class ImageViewer extends StatefulWidget {
+  const ImageViewer({Key? key, required this.file}) : super(key: key);
+  final DroppedFile? file;
+  @override
+  State<ImageViewer> createState() => _ImageViewerState();
+}
+
+class _ImageViewerState extends State<ImageViewer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: screenWidth(context, mulBy: 0.35),
+      height: screenHeight(context, mulBy: 0.4),
+      decoration: BoxDecoration(
+        color: Colors.blue
+      ),
+      child: (widget.file == null)?
+      const Center(
+          child: Text(
+            "No Preview",
+            style: TextStyle(color: Colors.white),
+          )):
+      Image.network(
+      widget.file!.url,
+      fit: BoxFit.contain,
+    ),
+    );
+  }
+}

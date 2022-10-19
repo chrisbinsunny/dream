b6bf8722904d51ae7e7f336143c8d56ecd7be5ee
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-10-20 02:34:27 +0530

    Update appBar.dart

--- lib/appBar.dart
+++ lib/appBar.dart
@@ -1,16 +1,19 @@
 
+import 'dart:ui';
+
+import 'package:color_finder/sizes.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class TopBarContents extends StatefulWidget {
-  final double opacity;
+class AppBarCustom extends StatefulWidget {
 
-  TopBarContents(this.opacity);
+  const AppBarCustom({super.key});
 
   @override
-  _TopBarContentsState createState() => _TopBarContentsState();
+  AppBarCustomState createState() => AppBarCustomState();
 }
 
-class _TopBarContentsState extends State<TopBarContents> {
+class AppBarCustomState extends State<AppBarCustom> {
   final List _isHovering = [
     false,
     false,
@@ -21,115 +24,119 @@ class _TopBarContentsState extends State<TopBarContents> {
     false,
     false
   ];
+  double opacity=0;
 
-  bool _isProcessing = false;
 
   @override
   Widget build(BuildContext context) {
-    var screenSize = MediaQuery.of(context).size;
+    opacity = Provider.of<ScrollDetail>(context, listen: true).getPos < screenHeight(context, mulBy: 0.1)
+        ? Provider.of<ScrollDetail>(context, listen: true).getPos / screenHeight(context, mulBy: 0.1)
+        : 1;
 
     return PreferredSize(
-      preferredSize: Size(screenSize.width, 1000),
-      child: Container(
-        color: Theme.of(context).bottomAppBarColor.withOpacity(widget.opacity),
-        child: Padding(
-          padding: EdgeInsets.all(20),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                'EXPLORE',
-                style: TextStyle(
-                  color: Colors.blueGrey[100],
-                  fontSize: 20,
-                  fontFamily: 'Montserrat',
-                  fontWeight: FontWeight.w400,
-                  letterSpacing: 3,
-                ),
-              ),
-              Expanded(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    SizedBox(width: screenSize.width / 8),
-                    InkWell(
-                      onHover: (value) {
-                        setState(() {
-                          value
-                              ? _isHovering[0] = true
-                              : _isHovering[0] = false;
-                        });
-                      },
-                      onTap: () {},
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Text(
-                            'Discover',
-                            style: TextStyle(
-                              color: _isHovering[0]
-                                  ? Colors.blue[200]
-                                  : Colors.white,
-                            ),
-                          ),
-                          SizedBox(height: 5),
-                          Visibility(
-                            maintainAnimation: true,
-                            maintainState: true,
-                            maintainSize: true,
-                            visible: _isHovering[0],
-                            child: Container(
-                              height: 2,
-                              width: 20,
-                              color: Colors.white,
-                            ),
-                          )
-                        ],
-                      ),
+      preferredSize: Size(screenWidth(context), 1000),
+      child: ClipRRect(
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: opacity*40, sigmaY: opacity*100, tileMode: TileMode.decal),
+          child: Container(
+            color: Colors.black.withOpacity(opacity/3),
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'DREAM',
+                    style: TextStyle(
+                      color: Colors.blueGrey[100],
+                      fontSize: 20,
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 5,
                     ),
-                    SizedBox(width: screenSize.width / 20),
-                    InkWell(
-                      onHover: (value) {
-                        setState(() {
-                          value
-                              ? _isHovering[1] = true
-                              : _isHovering[1] = false;
-                        });
-                      },
-                      onTap: () {},
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Text(
-                            'Contact Us',
-                            style: TextStyle(
-                              color: _isHovering[1]
-                                  ? Colors.blue[200]
-                                  : Colors.white,
-                            ),
+                  ),
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(width: screenWidth(context, mulBy: 0.125)),
+                        InkWell(
+                          onHover: (value) {
+                            setState(() {
+                              value
+                                  ? _isHovering[0] = true
+                                  : _isHovering[0] = false;
+                            });
+                          },
+                          onTap: () {},
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Text(
+                                'Color Finder',
+                                style: TextStyle(
+                                  color: _isHovering[0]
+                                      ? Colors.blue[200]
+                                      : Colors.white,
+                                ),
+                              ),
+                              SizedBox(height: 5),
+                              Visibility(
+                                maintainAnimation: true,
+                                maintainState: true,
+                                maintainSize: true,
+                                visible: _isHovering[0],
+                                child: Container(
+                                  height: 2,
+                                  width: 20,
+                                  color: Colors.white,
+                                ),
+                              )
+                            ],
                           ),
-                          SizedBox(height: 5),
-                          Visibility(
-                            maintainAnimation: true,
-                            maintainState: true,
-                            maintainSize: true,
-                            visible: _isHovering[1],
-                            child: Container(
-                              height: 2,
-                              width: 20,
-                              color: Colors.white,
-                            ),
-                          )
-                        ],
-                      ),
+                        ),
+                        SizedBox(width: screenWidth(context, mulBy: 0.05)),
+                        InkWell(
+                          onHover: (value) {
+                            setState(() {
+                              value
+                                  ? _isHovering[1] = true
+                                  : _isHovering[1] = false;
+                            });
+                          },
+                          onTap: () {},
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Text(
+                                'Gradient Finder',
+                                style: TextStyle(
+                                  color: _isHovering[1]
+                                      ? Colors.blue[200]
+                                      : Colors.white,
+                                ),
+                              ),
+                              SizedBox(height: 5),
+                              Visibility(
+                                maintainAnimation: true,
+                                maintainState: true,
+                                maintainSize: true,
+                                visible: _isHovering[1],
+                                child: Container(
+                                  height: 2,
+                                  width: 20,
+                                  color: Colors.white,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-              SizedBox(
-                width: screenSize.width / 50,
-              ),
-            ],
+            ),
           ),
         ),
       ),

c7d7462faeebc918ca31b950f22f00dcc91d5030
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-10-20 02:03:00 +0530

    Create appBar.dart

---
+++ lib/appBar.dart
@@ -0,0 +1,153 @@
+
+import 'package:flutter/material.dart';
+
+class TopBarContents extends StatefulWidget {
+  final double opacity;
+
+  TopBarContents(this.opacity);
+
+  @override
+  _TopBarContentsState createState() => _TopBarContentsState();
+}
+
+class _TopBarContentsState extends State<TopBarContents> {
+  final List _isHovering = [
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false
+  ];
+
+  bool _isProcessing = false;
+
+  @override
+  Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+
+    return PreferredSize(
+      preferredSize: Size(screenSize.width, 1000),
+      child: Container(
+        color: Theme.of(context).bottomAppBarColor.withOpacity(widget.opacity),
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'EXPLORE',
+                style: TextStyle(
+                  color: Colors.blueGrey[100],
+                  fontSize: 20,
+                  fontFamily: 'Montserrat',
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 3,
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(width: screenSize.width / 8),
+                    InkWell(
+                      onHover: (value) {
+                        setState(() {
+                          value
+                              ? _isHovering[0] = true
+                              : _isHovering[0] = false;
+                        });
+                      },
+                      onTap: () {},
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            'Discover',
+                            style: TextStyle(
+                              color: _isHovering[0]
+                                  ? Colors.blue[200]
+                                  : Colors.white,
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                          Visibility(
+                            maintainAnimation: true,
+                            maintainState: true,
+                            maintainSize: true,
+                            visible: _isHovering[0],
+                            child: Container(
+                              height: 2,
+                              width: 20,
+                              color: Colors.white,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: screenSize.width / 20),
+                    InkWell(
+                      onHover: (value) {
+                        setState(() {
+                          value
+                              ? _isHovering[1] = true
+                              : _isHovering[1] = false;
+                        });
+                      },
+                      onTap: () {},
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            'Contact Us',
+                            style: TextStyle(
+                              color: _isHovering[1]
+                                  ? Colors.blue[200]
+                                  : Colors.white,
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                          Visibility(
+                            maintainAnimation: true,
+                            maintainState: true,
+                            maintainSize: true,
+                            visible: _isHovering[1],
+                            child: Container(
+                              height: 2,
+                              width: 20,
+                              color: Colors.white,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: screenSize.width / 50,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+class ScrollDetail extends ChangeNotifier{
+  double scrollPosition = 0;
+
+
+  double get getPos{
+    return scrollPosition;
+  }
+
+  void setPos(double pos){
+    scrollPosition= pos;
+    notifyListeners();
+  }
+}

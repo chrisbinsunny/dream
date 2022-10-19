5fd68bcd1168e032bea6c92792a300b919e17dcc
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-10-19 20:28:57 +0530

    Update colorDetails.dart

--- lib/colorDetails.dart
+++ lib/colorDetails.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 import 'dropFile/dropFile.dart';
 
 class ColorDetails extends ChangeNotifier {
-  DroppedFile? file= DroppedFile(url: "#", mime: "image/jpeg", bytes: 171941, name: "img");
+  DroppedFile? file= DroppedFile(url: "#", mime: "image/jpeg", bytes: 171941, name: "Splash.jpg");
   Color color =Colors.black;
   Color colorTemp =Color(0xff8540ec);
   bool hover= false;

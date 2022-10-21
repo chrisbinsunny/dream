974aaba1d6c54f118115ad479fe9496c5fd69ad2
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-10-22 03:16:49 +0530

    Update footer.dart

--- lib/footer.dart
+++ lib/footer.dart
@@ -4,6 +4,9 @@ import 'package:color_finder/sizes.dart';
 import 'package:flutter/material.dart';
 import 'dart:html' as html;
 
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 class Footer extends StatelessWidget {
   const Footer({Key? key}) : super(key: key);
 
@@ -12,154 +15,206 @@ class Footer extends StatelessWidget {
     return ClipRRect(
       child: BackdropFilter(
         filter: ImageFilter.blur(sigmaX: 30.0, sigmaY: 30.0),
-        child: Container(
-          width: screenWidth(context,),
-          height: screenHeight(context, mulBy: 0.2),
-          constraints: const BoxConstraints(
-              minWidth: 400,
-              minHeight: 220
-          ),
-          alignment: Alignment.center,
-          padding: const EdgeInsets.symmetric(
-            vertical: 20,
-            horizontal: 50
-          ),
-          decoration: BoxDecoration(
-              color: Colors.black.withOpacity(0.4),
-              border: const Border(
-                top: BorderSide(
-                  color: Colors.white,
-                  width: 0.25
-                )
-              )
-          ),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            return Container(
+              width: screenWidth(context,),
+              height: screenHeight(context, mulBy: (constraints.biggest.width<850)?((constraints.biggest.width<580)?0.47 :0.37):0.2),
+              constraints: const BoxConstraints(
+                  minWidth: 400,
+                  minHeight: 220
+              ),
+              alignment: Alignment.center,
+              padding: const EdgeInsets.symmetric(
+                  vertical: 20,
+                  horizontal: 30
+              ),
+              decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.4),
+                  border: const Border(
+                      top: BorderSide(
+                          color: Colors.white,
+                          width: 0.25
+                      )
+                  )
+              ),
+              child: ListView(
+                shrinkWrap: false,
+                physics: const NeverScrollableScrollPhysics(),
                 children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'DREAM by Chrisbin',
-                        style: TextStyle(
-                          color: Colors.blueGrey[100],
-                          fontSize: 20,
-                          fontFamily: 'Montserrat',
-                          fontWeight: FontWeight.w600,
-                          letterSpacing: 5,
-                        ),
+                  Center(
+                    child: Text(
+                      'DREAM by Chrisbin',
+                      style: GoogleFonts.montserrat(
+                        color: Colors.blueGrey[100],
+                        fontSize: 25,
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: 5,
                       ),
-                      const SizedBox(height: 10,),
-                      SizedBox(
-                        width: 350,
-                        child:
-                        Text(
-                          "“Dream is not that which you see while sleeping it is something that does not let you sleep.”",
-                          style: TextStyle(
-                            color: Colors.blueGrey[100],
-                            fontFamily: 'Montserrat',
-                          ),
-                        ),
-                      ),
-                    ],
+                    ),
                   ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  const SizedBox(height: 10,),
+                  Center(
+                    child: Text(
+                      "Made using Flutter with 💙",
+                      style: GoogleFonts.montserrat(
+                          color: Colors.blueGrey[100],
+                          fontSize: 15
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 30,),
+                  Wrap(
+                    direction: Axis.horizontal,
+                    alignment: WrapAlignment.spaceEvenly,
+                    crossAxisAlignment: WrapCrossAlignment.start,
+                    runAlignment: WrapAlignment.spaceEvenly,
+                    runSpacing: 30,
+                    spacing: 20,
                     children: [
-                      TextButton(
-                        onPressed: (){
-                          html.window.open('https://github.com/chrisbinsunny/color-finder', 'new tab');
-                        },
-                        style: const ButtonStyle(
-                            alignment: Alignment.centerLeft
+                      SizedBox(
+                        width: 250,
+                        child:
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              "“Dream is not that which you see while sleeping it is something that does not let you sleep.”",
+                              style: TextStyle(
+                                color: Colors.blueGrey[100],
+                                fontFamily: 'Montserrat',
+                              ),
+                            ),
+                            const SizedBox(height: 5,),
+                            Text(
+                              "- APJ Abdul Kalam",
+                              textAlign: TextAlign.end,
+                              style: TextStyle(
+                                color: Colors.blueGrey[100],
+                                fontFamily: 'Montserrat',
+                              ),
+                            ),
+                          ],
                         ),
+                      ),
+                      Column(
+                        children: [
+                          TextButton(
+                            onPressed: (){
+                              html.window.open('https://github.com/chrisbinsunny/color-finder', 'new tab');
+                            },
+                            style: const ButtonStyle(
+                                alignment: Alignment.centerLeft
+                            ),
 
-                        child:  RichText(
-                            text: const TextSpan(
-                                text: "⭐ Find the code ",
-                                style: TextStyle(
-                                    fontSize: 14,
-                                    color: Colors.white
+                            child:  RichText(
+                                text: const TextSpan(
+                                    text: "Yay! This project is ",
+                                    style: TextStyle(
+                                        fontSize: 14,
+                                        color: Colors.white
+                                    ),
+                                    children: [
+                                      TextSpan(
+                                          text:"Open Source",
+                                          style: TextStyle(
+                                              color: Colors.grey,
+                                              decoration: TextDecoration.underline
+                                          )
+                                      ),
+                                      TextSpan(
+                                          text:" ✨",
+                                          style: TextStyle(
+                                            color: Colors.deepPurpleAccent,
+                                          )
+                                      ),
+                                    ]
+                                )),
+                          ),
+                          TextButton(
+                            onPressed: (){
+                              html.window.open('https://github.com/chrisbinsunny/color-finder', 'new tab');
+                            },
+                            style: const ButtonStyle(
+                                alignment: Alignment.centerLeft
+                            ),
+
+                            child:  RichText(
+                                text: const TextSpan(
+                                    text: "Report an issue or bug ",
+                                    style: TextStyle(
+                                        fontSize: 14,
+                                        color: Colors.white
+                                    ),
+                                    children: [
+                                      TextSpan(
+                                          text:"here",
+                                          style: TextStyle(
+                                              color: Colors.grey,
+                                              decoration: TextDecoration.underline
+                                          )
+                                      ),
+                                    ]
+                                )),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        width: 250,
+                        alignment: Alignment.center,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+
+                            Text(
+                              "Let's Chat!",
+                              style: GoogleFonts.fuzzyBubbles(
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.w600
+                              ),
+                            ),
+                            const SizedBox(height: 10,),
+                            Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                IconButton(
+                                    icon: const FaIcon(FontAwesomeIcons.twitter,),
+                                    onPressed: () {
+                                      html.window.open('https://twitter.com/chrisbinsunny', 'new tab');
+                                    }
                                 ),
-                                children: [
-                                  TextSpan(
-                                      text:"here",
-                                      style: TextStyle(
-                                          color: Colors.blueGrey,
-                                          decoration: TextDecoration.underline
-                                      )
-                                  ),
-                                ]
-                            )),
-                      ),
-                      TextButton(
-                        onPressed: (){
-                          html.window.open('https://github.com/chrisbinsunny/color-finder', 'new tab');
-                        },
-                        style: const ButtonStyle(
-                            alignment: Alignment.centerLeft
-                        ),
-
-                        child:  const Text(
-                          "Twitter",
-                          style: TextStyle(
-                              fontSize: 14,
-                              color: Colors.white,
-                              decoration: TextDecoration.underline
-                          ),
-                        ),
-                      ),
-                      TextButton(
-                        onPressed: (){
-                          html.window.open('https://github.com/chrisbinsunny/color-finder', 'new tab');
-                        },
-                        style: const ButtonStyle(
-                            alignment: Alignment.centerLeft
-                        ),
-
-                        child:  const Text(
-                          "Twitter",
-                          style: TextStyle(
-                              fontSize: 14,
-                              color: Colors.white,
-                              decoration: TextDecoration.underline
-                          ),
-                        ),
-                      ),
-                      TextButton(
-                        onPressed: (){
-                          html.window.open('https://github.com/chrisbinsunny/color-finder', 'new tab');
-                        },
-                        style: const ButtonStyle(
-                            alignment: Alignment.centerLeft
-                        ),
-
-                        child:  const Text(
-                          "Twitter",
-                          style: TextStyle(
-                              fontSize: 14,
-                              color: Colors.white,
-                              decoration: TextDecoration.underline
-                          ),
+                                IconButton(
+                                    icon: const FaIcon(FontAwesomeIcons.github,),
+                                    onPressed: () {
+                                      html.window.open('https://github.com/chrisbinsunny', 'new tab');
+                                    }
+                                ),
+                                IconButton(
+                                    icon: const FaIcon(FontAwesomeIcons.instagram,),
+                                    onPressed: () {
+                                      html.window.open('https://www.instagram.com/binary.ghost/', 'new tab');
+                                    }
+                                ),
+                                IconButton(
+                                    icon: const FaIcon(FontAwesomeIcons.linkedin,),
+                                    onPressed: () {
+                                      html.window.open('https://www.linkedin.com/in/chrisbin-sunny-279776171/', 'new tab');
+                                    }
+                                ),
+                              ],
+                            ),
+                          ],
                         ),
                       ),
                     ],
                   )
                 ],
               ),
-              const SizedBox(height: 10,),
-              Text(
-                "Made using Flutter with 💙",
-                style: TextStyle(
-                  color: Colors.blueGrey[100],
-                  fontFamily: 'Montserrat',
-                ),
-              ),
-
-            ],
-          ),
+            );
+          },
         ),
       ),
     );

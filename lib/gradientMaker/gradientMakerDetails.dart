638f6ee06e1d06c991858238d7c3f296f0f4afce
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-10-24 01:40:28 +0530

    Create gradientMakerDetails.dart

---
+++ lib/gradientMaker/gradientMakerDetails.dart
@@ -0,0 +1,23 @@
+
+
+
+import 'package:flutter/material.dart';
+
+
+class ColorDetails extends ChangeNotifier {
+
+List<Color> grads= [
+  Color(0xFFEE950F),
+  Color(0xFF3F51B5)
+];
+
+List<Color> get getGrads{
+    return grads;
+  }
+
+  void setGrad1(Color grad, int index){
+    grads[index]=grad;
+    notifyListeners();
+  }
+
+}

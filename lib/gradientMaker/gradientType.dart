72af662da88910b14a889ed1eaf35664bbedfede
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-10-24 18:20:29 +0530

    Update gradientType.dart

--- lib/gradientMaker/gradientType.dart
+++ lib/gradientMaker/gradientType.dart
@@ -1,22 +1,19 @@
-import 'dart:developer';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../sizes.dart';
+import 'gradientMakerDetails.dart';
 
-class GradientType extends StatefulWidget {
+class GradientType extends StatelessWidget {
   GradientType({Key? key}) : super(key: key);
-
-  @override
-  State<GradientType> createState() => _GradientTypeState();
-}
-
-class _GradientTypeState extends State<GradientType> {
   final hovered=ValueNotifier<int>(-1);
+  int selected=0;
 
   @override
   Widget build(BuildContext context) {
+    selected= Provider.of<GradientMakerDetails>(context, listen: true).getGradType;
     return Container(
       width: screenWidth(context, mulBy: 0.115
       ),
@@ -30,50 +27,76 @@ class _GradientTypeState extends State<GradientType> {
       child: ValueListenableBuilder<int>(
         valueListenable: hovered,
         builder: (context, hover, child) {
-
           return Column(
             children: [
-              InkWell(
-                onTap: (){},
-                onHover: (hover1){
-                  if(hover1){
-                    hovered.value=0;
-                  }else{
-                    hovered.value=-1;
-                  }
-                },
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: BackdropFilter(
-                    filter: ImageFilter.blur(sigmaX: 60.0, sigmaY: 60.0),
-                    child: Container(
-                      width: screenWidth(context, mulBy: 0.115
-                      ),
-                      height: screenHeight(context, mulBy: 0.06
-                      ),
-                      constraints: const BoxConstraints(
-                        minWidth: 210,
-                        minHeight: 50
-                      ),
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        color: Colors.black.withOpacity(0.2),
-                        borderRadius: BorderRadius.circular(15),
-                        border: Border.all(
-                            color: Colors.white.withOpacity(
-                                hover==0?1:
-                                0.5
-                            ),
-                            width: 2.5
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              )
+              GradientTypeButton(hovered: hovered, selected: selected, ),
             ],
           );
         },
+      ),
+    );
+}
+}
+
+class GradientTypeButton extends StatelessWidget {
+  const GradientTypeButton({
+    Key? key,
+    required this.hovered,
+    required this.selected,
+  }) : super(key: key);
+
+  final ValueNotifier<int> hovered;
+  final int selected;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: (){
+        Provider.of<GradientMakerDetails>(context, listen: false).setGradType(1);
+      },
+      onHover: (hover1){
+        if(hover1){
+          hovered.value=0;
+        }else{
+          hovered.value=-1;
+        }
+      },
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 60.0, sigmaY: 60.0),
+          child: Container(
+            width: screenWidth(context, mulBy: 0.115
+            ),
+            height: screenHeight(context, mulBy: 0.06
+            ),
+            constraints: const BoxConstraints(
+                minWidth: 210,
+                minHeight: 50
+            ),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: hovered.value==0?Colors.white.withOpacity(0.1):Colors.black.withOpacity(0.2),
+              borderRadius: BorderRadius.circular(15),
+              border: Border.all(
+                  color: Colors.white.withOpacity(
+                      selected==0?1:
+                      0.5
+                  ),
+                  width: 2.5
+              ),
+            ),
+            child: const Text(
+              "Linear Gradient",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w500,
+                fontSize: 18,
+                overflow: TextOverflow.clip,
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }

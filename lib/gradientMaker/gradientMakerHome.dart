50c05cdf87795ddb3c0b96ccdde3c03c3047dda1
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-10-26 18:32:45 +0530

    Update gradientMakerHome.dart

--- lib/gradientMaker/gradientMakerHome.dart
+++ lib/gradientMaker/gradientMakerHome.dart
@@ -6,8 +6,13 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../appBar.dart';
+import '../colorFinder/howToUse.dart';
 import '../footer.dart';
 import '../sizes.dart';
+import 'angleSelector.dart';
+import 'colorsView.dart';
+import 'gradientType.dart';
+import 'gradientView.dart';
 
 class GradientMakerHome extends StatefulWidget {
   const GradientMakerHome({Key? key,}) : super(key: key);
@@ -56,53 +61,53 @@ class _GradientMakerHomeState extends State<GradientMakerHome> {
                 controller: scrollController,
                 physics: const BouncingScrollPhysics(),
                 children: [
-                  // Container(
-                  //   alignment: Alignment.center,
-                  //   height: screenHeight(context, mulBy: 0.25),
-                  //   child: const Text(
-                  //     "Gradient Maker",
-                  //     style: TextStyle(
-                  //         color: Colors.white,
-                  //         fontSize: 30,
-                  //         fontWeight: FontWeight.bold
-                  //     ),
-                  //   ),
-                  // ),
-                  // Wrap(
-                  //   direction: Axis.horizontal,
-                  //   alignment: WrapAlignment.spaceEvenly,
-                  //   runSpacing: 20,
-                  //   spacing: 20,
-                  //   children:  [
-                  //     const GradientViewer(),
-                  //     Column(
-                  //       children: [
-                  //         const ColorsView(),
-                  //         const SizedBox(
-                  //           height: 20,
-                  //         ),
-                  //         SizedBox(
-                  //           width: screenWidth(context, mulBy: 0.3),
-                  //           child: ConstrainedBox(
-                  //             constraints: const BoxConstraints(
-                  //               minWidth: 530,
-                  //             ),
-                  //             child: Wrap(
-                  //               direction: Axis.horizontal,
-                  //               alignment: WrapAlignment.spaceEvenly,
-                  //               runSpacing: 20,
-                  //               spacing: 20,
-                  //               children: [
-                  //                 const AngleSelector(),
-                  //                 GradientType()
-                  //               ],
-                  //             ),
-                  //           ),
-                  //         )
-                  //       ],
-                  //     ),
-                  //   ],
-                  // ),
+                  Container(
+                    alignment: Alignment.center,
+                    height: screenHeight(context, mulBy: 0.25),
+                    child: const Text(
+                      "Gradient Maker",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold
+                      ),
+                    ),
+                  ),
+                  Wrap(
+                    direction: Axis.horizontal,
+                    alignment: WrapAlignment.spaceEvenly,
+                    runSpacing: 20,
+                    spacing: 20,
+                    children:  [
+                      const GradientViewer(),
+                      Column(
+                        children: [
+                          const ColorsView(),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          SizedBox(
+                            width: screenWidth(context, mulBy: 0.3),
+                            child: ConstrainedBox(
+                              constraints: const BoxConstraints(
+                                minWidth: 530,
+                              ),
+                              child: Wrap(
+                                direction: Axis.horizontal,
+                                alignment: WrapAlignment.spaceEvenly,
+                                runSpacing: 20,
+                                spacing: 20,
+                                children: [
+                                  const AngleSelector(),
+                                  GradientType()
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
                   const SizedBox(
                     height: 100,
                   ),

4ec8c79e7690ae6088fcb357fe1f4ea91cabaed2
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-10-28 02:52:09 +0530

    Update drawer.dart

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class DreamDrawer extends StatelessWidget {
   const DreamDrawer({Key? key}) : super(key: key);
@@ -8,10 +11,110 @@ class DreamDrawer extends StatelessWidget {
     return LayoutBuilder(
       builder: (context, constraints) {
       if(constraints.biggest.width<550){
-        return const Drawer();
+        return Drawer(
+          backgroundColor: Colors.black,
+          child: Column(
+            children: [
+              DrawerHeader(
+                child: Center(
+                  child: Text(
+                  'DREAM',
+                  style: GoogleFonts.montserrat(
+                    color: Colors.blueGrey[300],
+                    fontSize: 27,
+                    fontWeight: FontWeight.w600,
+                    letterSpacing: 5,
+                  ),
+              ),
+                ),
+
+              ),
+              ListTile(
+                title: Text(
+                    "Color Finder",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18
+                ),
+                ),
+                onTap: cfOnTap(context),
+                hoverColor:Colors.blueGrey[300]!.withOpacity(0.1),
+                focusColor: Colors.deepPurpleAccent.withOpacity(0.3),
+                enabled: true,
+                selectedTileColor: Colors.blueGrey[300]!.withOpacity(0.3),
+                selected: ((ModalRoute.of(context)!.settings.name=="/color-finder/")||
+                    (ModalRoute.of(context)!.settings.name=="/color-finder")||
+                    (ModalRoute.of(context)!.settings.name=="/")
+                ),
+                trailing: Icon(
+                  Icons.navigate_next,
+                  color: Colors.white,
+                ),
+              ),
+              ListTile(
+                title: Text(
+                  "Gradient Maker",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18
+                  ),
+                ),
+                onTap: gmOnTap(context),
+                hoverColor: Colors.blueGrey[300]!.withOpacity(0.1),
+                focusColor: Colors.deepPurpleAccent.withOpacity(0.3),
+                enabled: true,
+                selectedTileColor: Colors.deepPurpleAccent.withOpacity(0.3),
+                selected: ((ModalRoute.of(context)!.settings.name=="/gradient-maker/")||
+        (ModalRoute.of(context)!.settings.name=="/gradient-maker")
+        ),
+                trailing: Icon(
+                  Icons.navigate_next,
+                  color: Colors.white,
+                ),
+              )
+            ],
+          ),
+        );
       }
       return SizedBox();
     },
     );
+  }
+
+
+
+  VoidCallback? gmOnTap(BuildContext context){
+    switch(ModalRoute.of(context)!.settings.name){
+      case "/gradient-maker":
+      case "/gradient-maker/":
+        return null;
+        break;
+      case "/color-finder":
+      case "/color-finder/":
+      case "/":
+        return (){
+          Navigator.of(context).pushNamed("/gradient-maker");
+        };
+        break;
+    }
+    return null;
+  }
+
+  VoidCallback? cfOnTap(BuildContext context){
+    switch(ModalRoute.of(context)!.settings.name){
+      case "/gradient-maker":
+      case "/gradient-maker/":
+        return (){
+          Navigator.of(context).pushNamed("/color-finder");
+        };
+        break;
+      case "/color-finder":
+      case "/color-finder/":
+      case "/":
+        return null;
+
+        break;
+    }
+    return null;
   }
 }

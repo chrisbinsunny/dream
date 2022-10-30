81b68ed92cdc7d5fda57748f3cfb0e90981c6de8
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-10-30 16:40:57 +0530

    Update analytics.dart

--- lib/analytics.dart
+++ lib/analytics.dart
@@ -32,10 +32,10 @@ class AnalyticsService extends ChangeNotifier{
     await analytics.logSignUp(signUpMethod: method);
   }
 
-  Future logColorCopy({required String color, bool hex=true}) async {
+  Future logColorCopy({bool hex=true}) async {
     await analytics.logEvent(
       name: 'Copy color',
-      parameters: {'color': color, 'hex': hex,},
+      parameters: {'type': hex?"Hex":"RGBA",},
     );
   }
 
@@ -46,10 +46,73 @@ class AnalyticsService extends ChangeNotifier{
     );
   }
 
-  Future logUploadImage({required String image, required String type,}) async {
+  Future logDragDrop({required String image, required String type,}) async {
     await analytics.logEvent(
-      name: 'Upload image',
+      name: 'Drag & Drop image',
       parameters: {'name': image, 'type': type,},
+    );
+  }
+
+  Future logSocial({required String social, }) async {
+    await analytics.logEvent(
+      name: 'Social clicked',
+      parameters: {'social': social,},
+    );
+  }
+
+  Future logLinkClicked({required String linkTitle, }) async {
+    await analytics.logEvent(
+      name: 'Link clicked',
+      parameters: {'link': linkTitle,},
+    );
+  }
+
+ /// ///////////////////////////////////////////////////////////////////////////////////////////////
+
+  Future logColorChanged() async {
+    await analytics.logEvent(
+      name: 'Gradient color changed',
+    );
+  }
+
+  Future logColorAdded() async {
+    await analytics.logEvent(
+      name: 'New gradient color',
+    );
+  }
+
+  Future logColorRemoved() async {
+    await analytics.logEvent(
+      name: 'Remove gradient color',
+    );
+  }
+
+  Future logAngleChanged() async {
+    await analytics.logEvent(
+      name: 'Angle changed',
+    );
+  }
+
+  Future logGradientTypeChanged({required String type, }) async {
+    await analytics.logEvent(
+      name: 'GradientType changed',
+      parameters: {'type': type,},
+    );
+  }
+
+  Future logShareCode({required int gradCount, required int type, bool copyCSS=false}) async {
+    String gradType="";
+    switch(type){
+      case 0: gradType="Linear Gradient";
+      break;
+      case 1: gradType="Radial Gradient";
+      break;
+      case 2: gradType="Sweep Gradient";
+      break;
+    }
+    await analytics.logEvent(
+      name: 'Design shared',
+      parameters: {'gradCount': gradCount, 'gradType': gradType, 'copyCSS': copyCSS},
     );
   }
 }
